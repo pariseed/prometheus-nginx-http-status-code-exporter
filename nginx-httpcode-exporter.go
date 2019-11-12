9c40f9e6d1460d324500ec354a2074a7ed729fc1
daniele-parise <danihp923@gmail.com>
2019-11-12 16:02:13 +0100

    Update nginx-httpcode-exporter.go

--- nginx-httpcode-exporter.go
+++ nginx-httpcode-exporter.go
@@ -14,7 +14,6 @@ import (
 )
 
 
-var s = []string{}
 
 var code200 = []int{}
 var code200count = 0
@@ -23,29 +22,21 @@ var code200count = 0
 func getlog() {
 
 	t, _ := tail.TailFile("/var/log/nginx/nginx-access.log", tail.Config{Follow: true})
+
 	for line := range t.Lines {
-	    	s = append(s, line.Text)
-	}
-}
 
-func parsecode() {
-	for elem := range s {
-
-		words := strings.Split(s[elem], "	")
+		words := strings.Split(line.Text, "	")
 		code, _ := strconv.Atoi(words[3])
 
 		if (code == 200) {
 
 			code200 = append(code200, code) 
-	
-
+//			fmt.Println(code)
 		}
+
 
 	}
 }
-
-
-
 
 var HTTP_200_CODE = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "HTTP_200_CODE",
@@ -57,25 +48,18 @@ func init() {
 	prometheus.MustRegister(HTTP_200_CODE)
 }
 
-
 func commit200() {
 
 	diff := math.Abs(float64(code200count)-float64(len(code200)))
 	//fmt.Printf("%.1f\n", x)
 	fmt.Printf("%d\n", int(diff))
 
-//	if (diff != 0) {
-
 	HTTP_200_CODE.Set(diff)
-//		HTTP_200_CODE.Set(diff)
-//	}// else {
-//		HTTP_200_CODE.Set(float64(len(code200)))
-//	}
 
 	code200count = len(code200)
 
 	code200 = code200[:0]
-//	s = s[:0]
+
 	time.Sleep(1 * time.Second)
 
 }
@@ -85,20 +69,17 @@ func exec() {
 	go getlog()
 
 	for {
-		parsecode()
 		go commit200()
 
-		time.Sleep(10 * time.Second)
+		time.Sleep(5 * time.Second)
 	}
 
 }
 
-
 func main() {
-
 	go exec()
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8005", nil))
-  
+
 }
